247b66bb4d1ac7535c3f0deac1eca6a46f78d16a
MeghaScaria <megha.s.scaria@gmail.com>
2026-08-19 01:12:38 +0530

    Refactor CustomerRepository with full CRUD support
    Refactored CustomerRepository.cs to implement a public class providing CRUD operations for Customer entities using Entity Framework. Added Add, GetAll, GetById, Search, Update, Delete, Exists, and Count methods. Updated namespaces for data context, interfaces, and models. Removed internal class definition and unused using statements.

--- Repositories/CustomerRepository.cs
+++ Repositories/CustomerRepository.cs
@@ -1,10 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using InfoMasterKonsole.Data;
+using InfoMasterKonsole.Interfaces;
+using InfoMasterKonsole.Models;
 
-namespace InfoMasterKonsole.Repositories
+namespace InfoMasterKonsole.Repositories;
+
+public class CustomerRepository : ICustomerRepository
 {
-    internal class CustomerRepository
+    public void Add(Customer customer)
     {
+        using (CustomerDbContext context = new CustomerDbContext())
+        {
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
+    }
+
+    public List<Customer> GetAll()
+    {
+        using (CustomerDbContext context = new CustomerDbContext())
+        {
+            return context.Customers.ToList();
+        }
+    }
+
+    public Customer GetById(int customerId)
+    {
+        using (CustomerDbContext context = new CustomerDbContext())
+        {
+            return context.Customers.Find(customerId);
+        }
+    }
+
+    public List<Customer> Search(string searchText)
+    {
+        using (CustomerDbContext context = new CustomerDbContext())
+        {
+            return context.Customers
+                .Where(c =>
+                    c.CustomerName.Contains(searchText) ||
+                    c.EmailAddress.Contains(searchText) ||
+                    c.PhoneNumber.Contains(searchText) ||
+                    c.CustomerType.Contains(searchText))
+                .ToList();
+        }
+    }
+
+    public void Update(Customer customer)
+    {
+        using (CustomerDbContext context = new CustomerDbContext())
+        {
+            context.Customers.Update(customer);
+            context.SaveChanges();
+        }
+    }
+
+    public void Delete(int customerId)
+    {
+        using (CustomerDbContext context = new CustomerDbContext())
+        {
+            Customer customer = context.Customers.Find(customerId);
+
+            if (customer != null)
+            {
+                context.Customers.Remove(customer);
+                context.SaveChanges();
+            }
+        }
+    }
+
+    public bool Exists(int customerId)
+    {
+        using (CustomerDbContext context = new CustomerDbContext())
+        {
+            return context.Customers.Any(c => c.CustomerId == customerId);
+        }
+    }
+
+    public int Count()
+    {
+        using (CustomerDbContext context = new CustomerDbContext())
+        {
+            return context.Customers.Count();
+        }
     }
 }
