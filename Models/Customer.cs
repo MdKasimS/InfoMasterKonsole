8730689cee2a60dc8ff9593758c95c563610cfdc
MeghaScaria <megha.s.scaria@gmail.com>
2026-08-19 01:10:08 +0530

    Add EF Core packages; refactor Customer entity class
    Added EF Core Design and Sqlite packages for DB support.
    Refactored Customer.cs: cleaned up usings, removed nested class,
    and defined public Customer class with relevant properties.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace InfoMasterKonsole.Models;
 
-namespace InfoMasterKonsole.Models
+public class Customer
 {
-    internal class Customer
-    {
-    }
+    public int CustomerId { get; set; }
+
+    public string CustomerName { get; set; } = "";
+
+    public string EmailAddress { get; set; } = "";
+
+    public string PhoneNumber { get; set; } = "";
+
+    public string Address { get; set; } = "";
+
+    public string CustomerType { get; set; } = "";
+
+    public DateTime RegistrationDate { get; set; }
 }
