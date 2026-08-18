2dd02a095e576e378e22c3372f51c97d0a64ab75
MeghaScaria <megha.s.scaria@gmail.com>
2026-08-19 01:15:05 +0530

    Redefine ICustomerService with customer management methods
    Updated ICustomerService to include methods for adding, retrieving, searching, updating, and deleting customers, as well as getting customer count. Removed previous empty/internal interface and unnecessary using directives.

--- Interfaces/ICustomerService.cs
+++ Interfaces/ICustomerService.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using InfoMasterKonsole.Models;
 
-namespace InfoMasterKonsole.Interfaces
+namespace InfoMasterKonsole.Interfaces;
+
+public interface ICustomerService
 {
-    internal interface ICustomerService
-    {
-    }
+    bool AddCustomer(Customer customer, out List<string> errors);
+
+    List<Customer> GetAllCustomers();
+
+    Customer GetCustomerById(int customerId);
+
+    List<Customer> SearchCustomers(string searchText);
+
+    bool UpdateCustomer(Customer customer, out List<string> errors);
+
+    bool DeleteCustomer(int customerId);
+
+    int GetCustomerCount();
 }
