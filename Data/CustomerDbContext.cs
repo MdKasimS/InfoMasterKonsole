8a2f02ef3a9e8e1e9356101cf150ac0971473bab
MeghaScaria <megha.s.scaria@gmail.com>
2026-08-19 01:10:45 +0530

    Refactor to use EF Core with SQLite for Customer context
    Refactored code to use Entity Framework Core by adding CustomerDbContext inheriting from DbContext, configured with SQLite (customers.db). Added Customers DbSet. Removed unused usings and old internal class.

--- Data/CustomerDbContext.cs
+++ Data/CustomerDbContext.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using InfoMasterKonsole.Models;
 
-namespace InfoMasterKonsole.Data
+namespace InfoMasterKonsole.Data;
+
+public class CustomerDbContext : DbContext
 {
-    internal class CustomerDbContext
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        optionsBuilder.UseSqlite("Data Source=customers.db");
     }
 }
